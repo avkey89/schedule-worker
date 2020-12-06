73e10e1045c9e2f3770a254bf0686d109ac9bc95
a.kluchnikov <fsfhg&@354wsP::$%Yaf!csvdvcDD>
2020-12-06 20:50:06 +0300

    fix api google calendar

--- src/Application/Service/GoogleApi/GoogleCalendar.php
+++ src/Application/Service/GoogleApi/GoogleCalendar.php
@@ -5,18 +5,27 @@ declare(strict_types=1);
 namespace App\Application\Service\GoogleApi;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Exception;
 
 class GoogleCalendar
 {
     public static function getHoliday()
     {
         $httpClient = new Client();
-        $response = $httpClient->get('https://www.googleapis.com/calendar/v3/calendars/ru.russian%23holiday%40group.v.calendar.google.com/events?key='.$_ENV['GOOGLE_API_CALENDAR_KEY'])->getBody()->getContents();
-
         $holidays = [];
-        $holidaysList = json_decode($response)->items;
-        foreach ($holidaysList as $day) {
-            $holidays[] = $day->start->date;
+
+        try {
+            $response =
+                $httpClient->get('https://www.googleapis.com/calendar/v3/calendars/ru.russian%23holiday%40group.v.calendar.google.com/events?key=' .
+                    $_ENV['GOOGLE_API_CALENDAR_KEY'])->getBody()->getContents();
+            $holidaysList = json_decode($response);
+
+            foreach ($holidaysList->items as $day) {
+                $holidays[] = $day->start->date;
+            }
+        } catch (GuzzleException $e) {
+        } catch (Exception $e) {
         }
 
         return $holidays;
